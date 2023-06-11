dbd6055d327d7d01e784e04c761e2f9b7a6a1937
nopyjf <rockon23146@gmail.com>
2023-06-12 03:14:16 +0700

    feat: add yup and hook form

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,86 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Controller, useForm } from "react-hook-form";
 import "./App.css";
-import { Typography } from "@mui/material";
+import { Autocomplete, Stack, TextField } from "@mui/material";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { InferType, number, object, string } from "yup";
+
+interface ITopFilm {
+  label: string;
+  year: number;
+}
+
+const schema = object({
+  label: string().required("Require Field"),
+  year: number().required(),
+}).required();
+
+type FormData = InferType<typeof schema>;
 
 function App() {
+  const top100Films: ITopFilm[] = [
+    { label: "The Shawshank Redemption", year: 1994 },
+    { label: "The Godfather", year: 1972 },
+    { label: "The Godfather: Part II", year: 1974 },
+    { label: "The Dark Knight", year: 2008 },
+    { label: "12 Angry Men", year: 1957 },
+    { label: "Schindler's List", year: 1993 },
+    { label: "Pulp Fiction", year: 1994 },
+    {
+      label: "The Lord of the Rings: The Return of the King",
+      year: 2003,
+    },
+    { label: "The Good, the Bad and the Ugly", year: 1966 },
+    { label: "Fight Club", year: 1999 },
+  ];
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    mode: "onChange",
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (data: any) => {
+    // Autofill Data and Validate it !
+    console.log(data);
+  };
+
   return (
     <>
-      
+      <Stack spacing={2} sx={{ width: 300 }}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Controller
+            name="label"
+            control={control}
+            render={({ field: { value, onChange, ref } }) => {
+              console.log(value);
+              console.log(onChange);
+              console.log(errors);
+              return (
+                <Autocomplete
+                  id="label"
+                  freeSolo
+                  options={top100Films.map((option) => option.label)}
+                  onChange={onChange}
+                  onBlur={onChange}
+                  renderInput={(params) => (
+                    <TextField
+                      {...params}
+                      value={value}
+                      label="Movie"
+                      inputRef={ref}
+                      error={!!errors.label}
+                      helperText={errors.label?.message}
+                    />
+                  )}
+                />
+              );
+            }}
+          />
+        </form>
+      </Stack>
     </>
   );
 }
